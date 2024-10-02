99868fed8d18162f0de27c8f120dfec37b9c40f2
Simon Jonsson <simonjonsson2002@gmail.com>
2024-10-02 14:50:10 +0200

    Vec3 now operational
    Also glm has been moved

--- source/main.cpp
+++ source/main.cpp
@@ -3,33 +3,39 @@
 #include "vec3.h"
 #include "ray.h"
 #include "color.h"
+#include <glm.hpp>
+#include <vector>
+
 //simpel kamera structur
+
 struct Camera
 {   
     //2d array som lagrar alla pixlar. Just nu 500x500
     //ska ändras till vec3
-    int Picture[500][500];
+    std::vector<std::vector<glm::vec3>> Picture;
+    int camerasize;
+
 };
 
 int main()
 {
-
     //Kameran som kommer rendreras
     Camera camera;
 
     //storlek på antal kolumner och rader i bilden
-    int cols = sizeof(camera.Picture[0]) / sizeof(camera.Picture[0][0]);
-    int rows = sizeof(camera.Picture) / sizeof(camera.Picture[0]);
+    camera.camerasize = 500;
 
-    //for-loop som går genom alla pixlar
-    //ska sedan användas för att skjuta rays
-    for (int Pixelx = 0; Pixelx < cols; Pixelx++)
-    {
-        for (int Pixely = 0; Pixely < rows; Pixely++)
+    //for-loop som skapar en blank bild
+    for (int Pixelx = 0; Pixelx < camera.camerasize; Pixelx++)
+    {   
+        //Skapar en rad
+        camera.Picture.push_back(std::vector<glm::vec3>());
+
+        for (int Pixely = 0; Pixely < camera.camerasize; Pixely++)
         {   
+            //skapar all kolumner
+            camera.Picture[Pixelx].push_back(glm::vec3(0, 0, 0));
             //Temporärt test för att se att den fungerade som det ska
-            //std::cout << "Hello World \n" << Pixelx;
-
         }
     }
 
